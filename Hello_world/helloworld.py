fbde0bd3c923ac20de3529a188a152b3faa15089
vorteg <vorteg.r@gmail.com>
2020-01-02 15:00:39 -0600

    py 3 y caracterees especiales

--- Hello_world/helloworld.py
+++ Hello_world/helloworld.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 # -*- coding: latin-1 -*-
-""" print "Esta es una forma de imprimir en py version 2.7 sin parentesis pero no es compatible con py 3.7" 
-otra cosa a resaltar es siempre poner el interprete a usar como bna practica y el lenguaje a usar para evitar errores de caracter especial o unicode 
-python soporta algunas buenas practicas de python 3.7 por lo que es recomendable trabajar con python 3.7 """
+""" print "He camboiado interprete, uso caracteres especiales como asentos y la bna practica de usar parentesis"""
 
-print "Esta es una forma de imprimir en py version 2.7"
+print ("Está es una forma de imprimir en py version 2.7")
