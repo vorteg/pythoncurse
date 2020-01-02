c83f22f01a4a89329be4f3cf52ff98cc432dbe8a
vorteg <vorteg.r@gmail.com>
2020-01-02 14:55:24 -0600

    Hello, con python 2.7

---
+++ Hello_world/helloworld.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2.7
+# -*- coding: latin-1 -*-
+""" print "Esta es una forma de imprimir en py version 2.7 sin parentesis pero no es compatible con py 3.7" 
+otra cosa a resaltar es siempre poner el interprete a usar como bna practica y el lenguaje a usar para evitar errores de caracter especial o unicode 
+python soporta algunas buenas practicas de python 3.7 por lo que es recomendable trabajar con python 3.7 """
+
+print "Esta es una forma de imprimir en py version 2.7"
